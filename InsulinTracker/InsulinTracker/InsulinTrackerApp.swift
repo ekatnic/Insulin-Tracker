4a63a25309cb2b9aebc21838e48051864ac2b384
Ethan <ethkatnic@gmail.com>
2023-10-31 22:23:23 -0700

    Upload proj skeleton

---
+++ InsulinTracker/InsulinTracker/InsulinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsulinTrackerApp.swift
+//  InsulinTracker
+//
+//  Created by Ethan Katnic on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct InsulinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
