c85028faa7bd50402d46251d2c37d8b2b1a352eb
Ethan <ethkatnic@gmail.com>
2023-11-02 19:39:49 -0700

    Start UI components

--- InsulinTracker/InsulinTracker/ContentView.swift
+++ InsulinTracker/InsulinTracker/ContentView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                EntryHeader()
+                VStack{
+                    TimeSelector()
+                    EntryTypeSelector()
+                    BloodSugarSelector()
+                    EnteredBySelector()
+                    ValidatedBySelector()
+                    Note()
+                    Button(action: {}) {
+                        Text("Enter")
+                    }
+                    Spacer()
+                }
+
+            }
         }
         .padding()
     }
@@ -22,5 +34,99 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+struct EntryHeader: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Image(systemName:"pencil").resizable().frame(width:30,height:20)
+                Text("Entry").font(.system(size:30, weight: .medium))
+            }
+        }.padding(.bottom)
+    }
+}
+
+struct TimeSelector: View {
+    var body: some View {
+        VStack{
+            DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { Text("Time").font(.system(size:24, weight: .medium)) })
+        }.padding()
+    }
+}
+
+struct EntryTypeSelector: View {
+    var body: some View {
+        VStack{
+            Text("Entry Type").font(.system(size:24, weight: .medium)).frame(maxWidth: .infinity, alignment: .leading)
+            
+        }.padding()
+        ControlGroup {
+            Button(action: {}) {
+                Text("Breakfast")
+            }
+            Button(action: {}) {
+                Text("Lunch")
+            }
+            Button(action: {}) {
+                Text("Dinner")
+            }
+            Button(action: {}) {
+                Text("Other")
+            }
+        }
+    }
+}
+
+struct BloodSugarSelector: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Blood Sugar Level").font(.system(size:24, weight: .medium))
+                TextField("Level", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                
+            }
+        }.padding()
+    }
+}
+
+struct EnteredBySelector: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Entered By").font(.system(size:24, weight: .medium))
+                Spacer()
+                TextField("Name", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            }
+        }.padding()
+    }
+}
+
+struct ValidatedBySelector: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Validated By").font(.system(size:18, weight: .medium))
+                TextField("Name", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Text("Pin").font(.system(size:24, weight: .medium))
+                TextField("Pin", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                Spacer()
+            }
+        }.padding()
+    }
+}
+
+struct Note: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Note").font(.system(size:24, weight: .medium)).padding(.leading).frame(maxWidth: .infinity, alignment: .leading)
+            }
+            TextEditor(text: .constant("Write your note here..."))
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Insulin-Tracker
+
+<h3> Entry Page Wire Frame </h3>
+<img src="EntryWireFrame.png">
