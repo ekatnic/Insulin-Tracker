56858a5aacc3e662678b0c75255e76c4bd17a494
Ethan <ethkatnic@gmail.com>
2023-11-08 22:07:25 -0800

    Remove unused line

--- InsulinTracker/InsulinTracker/ContentView.swift
+++ InsulinTracker/InsulinTracker/ContentView.swift
@@ -34,7 +34,6 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            Color(.blue)
             VStack {
                 NavigationStack{
                     VStack{
